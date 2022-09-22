28ce190ada9a1df3b806ec5fac35f25476f2a892
Amirhosein Asghari <amirhoseinasghari2000@gmail.com>
2022-09-22 14:45:13 +0330

    routes created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+//components
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Exchanges from "./components/Exchanges";
+import Cryptocurrencies from "./components/Cryptocurrencies";
+import CryptoDetails from "./components/CryptoDetails";
+import News from "./components/News";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+        <Route path="/exchanges" element={<Exchanges />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/crypto/:id" element={<CryptoDetails />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
